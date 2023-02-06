c99ac3284336d5791f5a15fd78ff2cc951cd3939
NandhakumarRNK <nandha28052000@gmail.com>
2023-02-06 16:28:37 +0530

    init

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-reportWebVitals(console.log); //performance analyzer
+reportWebVitals(console.log);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
